0164d39f5e71b88da17e9343fc7d7c7841d3e4dc
mugiwara <mugiwara@localhost.localdomain>
2022-11-03 10:09:21 +0700

    fixing go get

---
+++ pkg/config/app.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	//  dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	d, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/simplerest_bookstore?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -1,12 +1,16 @@
 package routes
 
 import (
-	"github.com/BintangDiLangit/go-bookstore/pkg/controllers"
+	"github.com/bintangdilangit/go-bookstore/pkg/controllers"
 	"github.com/gorilla/mux"
 )
 
 var RegisterBookStoreRoutes = func(router *mux.Router) {
-	router.HandleFunc("/book/", controllers.CreateBook)
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
 }
 
 // func ()  {
